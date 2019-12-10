44e502a2c0d8652441028cad36c4da27f667f8c1
tzbd1819 <1819434425@qq.com>
2019-12-10 15:40:53 +0800

    Create Target.java

---
+++ Target.java
@@ -0,0 +1,13 @@
+@Target(ElementType.TYPE)
+public @interface Table {
+    /**
+     * 数据表名称注解，默认值为类名称
+     * @return
+     */
+    public String tableName() default "className";
+}
+
+@Target(ElementType.FIELD)
+public @interface NoDBColumn {
+
+}
